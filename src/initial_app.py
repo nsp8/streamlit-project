998f255c5158ddcca8c548219f139a236f3a91f1
Nishant Parmar <nishant.singhparmar@ge.com>
2024-07-05 11:54:21 -0400

    Initial: Adding a map chart

---
+++ src/initial_app.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import streamlit
+
+
+def plot_map() -> None:
+    streamlit.write("Plotting a Map")
+    map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        columns=["lat", "lon"]
+    )
+    streamlit.map(map_data)
+
+
+if __name__ == "__main__":
+    plot_map()
