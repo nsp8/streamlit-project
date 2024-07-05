5db2509543d798857da4ad3f2b3dbf7dbccbe9fb
Nishant Parmar <nishant.singhparmar@ge.com>
2024-07-05 13:12:28 -0400

    1. Adding widget 2. Making app cleaner, more robust

--- src/basic_app.py
+++ src/basic_app.py
@@ -1,19 +1,49 @@
-import numpy as np
-import pandas as pd
+from numpy.random import randint, randn
+from pandas import DataFrame
 import streamlit
 
 
-def plot_map() -> None:
-    streamlit.write("Plotting a Map")
-    map_data = pd.DataFrame(
-        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-        columns=["lat", "lon"]
-    )
-    streamlit.map(map_data)
+class MapDashboard:
+    def __init__(
+        self,
+        num_points: int = 1000,
+        lat_min: int = 7,
+        lat_max: int = 85,
+        lon_min: int = -20,
+        lon_max: int = -179,
+    ):
+        self.error = False
+        try:
+            self.lat_sidebar: tuple = streamlit.sidebar.slider(
+                "Latitude", lat_min, lat_max, (lat_min, lat_max)
+            )
+            self.lon_sidebar: tuple = streamlit.sidebar.slider(
+                "Longitude", lon_min, lon_max, (lon_min, lon_max)
+            )
+            self.data = self.get_data(num_points, self.lat_sidebar, self.lon_sidebar)
+        except ValueError:
+            streamlit.write("Invalid range selected!")
+            self.error = True
 
+    @staticmethod
+    def get_data(num_points: int, lat_range: tuple, lon_range: tuple) -> DataFrame:
+        lat_choice = randint(*lat_range)
+        lon_choice = randint(*lon_range)
+        streamlit.write(f"Coordinates around: ({lat_choice}, {lon_choice})")
+        return DataFrame(
+            randn(num_points, 2) / [50, 50] + [lat_choice, lon_choice],
+            columns=["lat", "lon"]
+        )
 
-def sidebar():
+    def show_map(self) -> None:
+        try:
+            streamlit.map(self.data)
+        except AttributeError:
+            streamlit.write("Couldn't generate data for given range.")
+        else:
+            if not self.error:
+                streamlit.write("Plotting points on the 🌎")
 
 
 if __name__ == "__main__":
-    plot_map()
+    MapDashboard().show_map()
